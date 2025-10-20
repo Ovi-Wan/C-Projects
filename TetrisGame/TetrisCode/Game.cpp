bfaf8a20c19eb4dd8f017346a2cb7055bcff177f
Ovi-Wan <allfootball940@gmail.com>
2025-10-20 13:00:34 +0200

    update

--- TetrisGame/TetrisCode/Game.cpp
+++ TetrisGame/TetrisCode/Game.cpp
@@ -14,14 +14,92 @@ Game::Game()
         }
     }
 
+    currentTetro = nullptr;
+    currentRow = 0;
+    currentCol = WIDTH / 2-2;
+
+
     cout << "Loading....\n";
 
+}
+
+Game::~Game()
+{
+    if (currentTetro)
+        delete currentTetro;
+}
+
+void Game::clearTetromino(const Tetromino& tetro, int row, int col)
+{
+    for (int i = 0, i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            if (tetro.shape[i][j] != ' ')
+            {
+                int x = row + i;
+                int y = col`+ j;
+
+                if (x >= 0 && x < HEIGHT && y >= 0 && y < WIDTH)
+                    grid[x][y] = ' ';
+
+            }
+        }
+    }
+}
+
+bool Game::canMove(const Tetromino& tetro, int newRow, int newCol)
+{
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            if (tetro.shape[i][j] != '')
+            {
+                int x = newRow + i;
+                int y = newCol + j;
+
+                                if (x < 0 || x >= HEIGHT || y < 0 || y >= WIDTH)
+                    return false;
+
+               
+                if (grid[x][y] != ' ' && !(x >= currentRow && x < currentRow + 4 && y >= currentCol && y < currentCol + 4))
+                    return false;
+            }
+        }
+    }
+    return true;
+}
+
+void Game::moveDown()
+{
+    if (!currentTetro) return;
+
+    if (canMove(*currentTetro, currentRow + 1, currentCol))
+    {
+        clearTetromino(*currentTetro, currentRow, currentCol);
+        currentRow++;
+        placeTetromino(*currentTetro, currentRow, currentCol);
+    }
+    else
+    {
+       
+        delete currentTetro;
+        currentTetro = new Tetromino('T');
+        currentRow = 0;
+        currentCol = WIDTH / 2 - 2;
+
+        placeTetromino(*currentTetro, currentRow, currentCol);
+    }
 }
 
 
 void Game::run()
 {
     cout << "Game Started\n";
+
+    Tetromino t('T');
+    placeTetromino(t, 0, 3);
     draw();
 
 }
@@ -44,4 +122,27 @@ void Game::draw()
     }
 
     cout << "+------------+\n";
+}
+
+
+void Game::placeTetromino(const Tetromino& tetro, int row, int col)
+{
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            if (tetro.shape[i][j] != ' ')
+            {
+                int x = row + i;
+                int y = col + j;
+
+                if (x >= 0 && x < HEIGHT && y >= 0 && < WIDTH)
+                {
+                    grid[x][y] = tetro.shape[i][j];
+
+                }
+
+            }
+        }
+    }
 }
